c2decc57d7a01ad6377f467a616711b42347b5b6
yoshi <yoshi.trd865@gmail.com>
2026-08-16 15:08:13 +0900

    ふせんくんの表示

--- DotNetBridge/wwwroot/js/custom-inject.js
+++ DotNetBridge/wwwroot/js/custom-inject.js
@@ -21,6 +21,8 @@ function runIfEnabled(featureId, action) {
         console.log(`[ProxyInject] ${featureId} は設定でOFFのためスキップ`);
     }
 }
+// ★ initFusenKun は observeDOM の外側で1回だけ起動させる！
+runIfEnabled("fusen_kun", initFusenKun);
 
 observeDOM(() => {
     // ⚙️ メニュー画面のカスタマイズカード表示（これは常に起動）
@@ -48,5 +50,4 @@ observeDOM(() => {
     // 📸 点検BOXワープ ＆ 戻るボタン修復（★ここに追加！）
     runIfEnabled("tenkenbox_worp", initInspectionWarp);
     runIfEnabled("zandaka_copy", initZandakaCopy);
-    runIfEnabled("fusen_kun", initFusenKun);
 });
